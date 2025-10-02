fe40a1f26c06a6fe08d9e804f4cdef991205a2fa
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-02 02:49:27 -0400

    Refactor index.ts to streamline user data handling by updating import statements and simplifying database queries

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
 import "dotenv/config";
 import { db } from "./db.js";
 import { eq } from "drizzle-orm";
-import { usersTable as users } from "./db/schema.js";
+import { usersTable } from "./db/schema.js";
 import { uuid } from "@electric-sql/pglite";
 
-await db
-  .insert(users)
-  .values({
-    id: uuid(),
-    email: "cacheman@upstash.com",
-    name: "Cache Man",
-    age: 100,
-  });
+await db.insert(usersTable).values({
+  id: uuid(),
+  email: "cacheman@upstash.com",
+  name: "Cache Man",
+  age: 100,
+});
 
-const user1 = await db
-  .select()
-  .from(users)
-  .where(eq(users.email, "cacheman@upstash.com"))
-  .$withCache();
+const users = await db.select().from(usersTable).$withCache();
 
 // await db.$cache.invalidate({ tables: users });
 
-await db
-  .update(users)
-  .set({ name: "wowman" })
-  .where(eq(users.id, user1[0]?.id!));
+// await db
+//   .update(users)
+//   .set({ name: "wowman" })
+//   .where(eq(users.id, user1[0]?.id!));
 
 console.log(
   JSON.stringify(
     {
-      user1,
+      users,
     },
     null,
     2
