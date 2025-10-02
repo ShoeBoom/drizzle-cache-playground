0b2b46ce4bb4ae6a53bbe5c89b0658c05439ded1
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-02 02:26:37 -0400

    Refactor package.json and db.ts; remove pgserver-run script

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,19 @@
+import { PGlite } from "@electric-sql/pglite";
 import "dotenv/config";
 import { upstashCache } from "drizzle-orm/cache/upstash";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/pglite";
+
+const client = new PGlite();
 
 export const db = drizzle({
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-    },
-    cache: upstashCache({
-        url: process.env.UPSTASH_REDIS_REST_URL!,
-        token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-        // 👇 Enable caching for all queries by default (optional)
-        global: true,
+  client,
+  cache: upstashCache({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+    // 👇 Enable caching for all queries by default (optional)
+    global: true,
 
-        // 👇 Default cache behavior (optional)
-        config: { ex: 60 * 10 }
-    }),
-
+    // 👇 Default cache behavior (optional)
+    config: { ex: 60 * 10 },
+  }),
 });
