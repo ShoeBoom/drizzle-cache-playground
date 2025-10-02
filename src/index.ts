d808fd227810e870872367f3c6cef70be5fe3fd6
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-02 02:45:33 -0400

    Refactor index.ts to include UUID generation for new user and update query structure for better readability

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
 import "dotenv/config";
 import { db } from "./db.js";
 import { eq } from "drizzle-orm";
-import { usersTable as users} from "./db/schema.js";
+import { usersTable as users } from "./db/schema.js";
+import { uuid } from "@electric-sql/pglite";
 
-// await db.insert(usersTable).values({email: "cacheman@upstash.com", name: "Cache Man", age: 100 });
+await db
+  .insert(users)
+  .values({
+    id: uuid(),
+    email: "cacheman@upstash.com",
+    name: "Cache Man",
+    age: 100,
+  });
 
-const user1 = await db.select().from(users).where(eq(users.id, "0465dbe5-9f8e-4eab-15f6-aec8d6db8fbe")).$withCache();
-const user2 = await db.select().from(users).where(eq(users.id, "0b8d2913-1e1d-4a3a-e746-4525a4bf0131")).$withCache();
+const user1 = await db
+  .select()
+  .from(users)
+  .where(eq(users.email, "cacheman@upstash.com"))
+  .$withCache();
 
 // await db.$cache.invalidate({ tables: users });
 
-await db.update(users).set({name: "wowman"}).where(eq(users.id, user1[0]?.id!))
+await db
+  .update(users)
+  .set({ name: "wowman" })
+  .where(eq(users.id, user1[0]?.id!));
 
-console.log(JSON.stringify({
-    user1,
-    user2
-}, null, 2));
+console.log(
+  JSON.stringify(
+    {
+      user1,
+    },
+    null,
+    2
+  )
+);
